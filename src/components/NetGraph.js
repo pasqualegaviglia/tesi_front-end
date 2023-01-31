c1139824334b7eb38f6ce0205e88b8b76600f83c
pasqualegaviglia <p.gaviglia@studenti.unina.it>
2023-01-31 11:22:27 +0100

    terminata automizzazione

--- src/components/NetGraph.js
+++ src/components/NetGraph.js
@@ -6,17 +6,12 @@ function NetGraph()
   var prova = [];
 
   prova.push({id: 1, label: "Node 1", title: "node 1 tootip text"});
+  prova.push({id: 2, label: "Node 2", title: "node 2 tootip text"});
 
     const graph = 
     {
-        nodes: 
-        [
-          prova[0],
-          { id: 2, label: "Node 2", title: "node 2 tootip text" },
-          { id: 3, label: "Node 3", title: "node 3 tootip text" },
-          { id: 4, label: "Node 4", title: "node 4 tootip text" },
-          { id: 5, label: "Node 5", title: "node 5 tootip text" }
-        ],
+        nodes: prova,
+        
         edges: 
         [
           { from: 1, to: 2 },
