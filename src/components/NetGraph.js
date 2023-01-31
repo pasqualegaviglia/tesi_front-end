102ac52a149926fd9f290b6de03650d6f1cb1e3e
pasqualegaviglia <p.gaviglia@studenti.unina.it>
2023-01-31 10:46:28 +0100

    inizio grafico

--- src/components/NetGraph.js
+++ src/components/NetGraph.js
@@ -1,9 +1,53 @@
 import React from "react";
+import Graph from "react-graph-vis";
 
 function NetGraph()
 {
+    const graph = 
+    {
+        nodes: [
+          { id: 1, label: "Node 1", title: "node 1 tootip text" },
+          { id: 2, label: "Node 2", title: "node 2 tootip text" },
+          { id: 3, label: "Node 3", title: "node 3 tootip text" },
+          { id: 4, label: "Node 4", title: "node 4 tootip text" },
+          { id: 5, label: "Node 5", title: "node 5 tootip text" }
+        ],
+        edges: [
+          { from: 1, to: 2 },
+          { from: 1, to: 3 },
+          { from: 2, to: 4 },
+          { from: 2, to: 5 }
+        ]
+      };
+
+    const options = 
+    {
+        edges: {
+            color: "#000000",
+            smooth: {
+              type: "cubicBezier",
+              forceDirection: "none"
+            }
+        },
+        physics: {
+            enabled: false,
+            minVelocity: 0.75
+        },
+        height: "500px"
+    };
+
+    const events = {
+        select(event) {
+          const { nodes, edges } = event;
+          console.log('Selected nodes:');
+          console.log(nodes);
+          console.log('Selected edges:');
+          console.log(edges);
+        },
+      };
+
     return(
-        <h1>prova</h1>
+        <Graph graph={graph} options={options} events={events} />
     )
 }
 
